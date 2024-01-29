ced400ebe8415cbb424384697c4b235575c19862
Severin Buhler <sevi_buehler@hotmail.com>
2024-01-29 11:58:12 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,55 @@
-import Image from "next/image";
+'use client';
+
+import React from "react";
 import styles from "./page.module.css";
+import { BlocktankClient } from "@synonymdev/blocktank-lsp-http-client";
 
 export default function Home() {
+  const [value, setValue] = React.useState<string>();
+
+  class BlocktankUrl {
+    static localhost = 'http://localhost:9000/api';
+    static staging = "https://api.stag.blocktank.to/blocktank/api/v2/";
+  }
+
+  const blocktankUrl = BlocktankUrl.staging;
+
+  async function pullBlocktank() {
+    const client = new BlocktankClient(blocktankUrl)
+    const info = await client.getInfo();
+    setValue(JSON.stringify(info, null, 2));
+  }
+
+  async function createOrder() {
+    console.log('Create order click')
+    const client = new BlocktankClient(blocktankUrl)
+    try {
+      const order = await client.createOrder(1000000000, 12);
+      console.log('order', order)
+    } catch (e) {
+      console.error('Order creation failed', e)
+    }
+  }
+
+  React.useEffect(() => {
+    pullBlocktank()
+
+  }, [])
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <p>Test blocktank client {blocktankUrl}</p>
+          <pre>{value}</pre>
+
+          <button onClick={() => {
+            createOrder()
+          }}>
+            Create Order
+          </button>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
